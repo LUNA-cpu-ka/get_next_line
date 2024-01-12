3b3994ad397e04bf0d7a950130349cf88721e211
saait-si <sajidaaitsimhand1@gmail.com>
2024-01-12 15:04:36 +0100

    afk

---
+++ get_next_line_bonus.c
@@ -0,0 +1,43 @@
+
+
+#include "get_next_line_bonus.h"
+
+char	*ft_read_to_temp(int fd, char *temp)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(temp, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			free(temp);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		temp = ft_strjoin(temp, buff);
+	}
+	free(buff);
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*temp[FD_SETSIZE];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= FD_SETSIZE)
+		return (NULL);
+	temp[fd] = ft_read_to_temp(fd, temp[fd]);
+	if (!temp[fd])
+		return (NULL);
+	line = ft_get_line(temp[fd]);
+	temp[fd] = ft_new_temp(temp[fd]);
+	return (line);
+}
